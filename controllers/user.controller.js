25fd6aafbb729afe87add424a08658e6a4d67a00
aman03trave <aman.panda@learner.manipal.edu>
2025-03-12 21:09:48 +0530

    4th

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,8 +4,9 @@ const user = new Users();
 
 export const RegisterUser = async ( req, res, next ) => {
     try {
-        const { name, email, password } = req.body;
-        await user.createUser(name, email, password);
+        const { name, gender, age, email, password, role_name } = req.body;
+        const role_id = await user.getRole_id(role_name);
+        await user.createUser(name, gender, age, email, password, role_id);
         res.json({ status: true, message: 'User registered successfully' });
     } catch (err) {
         next(err);
--- services/user.service.js
+++ services/user.service.js
@@ -8,13 +8,18 @@ class Users {
     async createUser(name, email, password) {
         const id = parseInt(name.length + Math.random() * 1000);
         await pool
-           .query('INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)', [id, name, email, password])
+           .query('INSERT INTO Users (id, name, gender, age, email, password, role_id) VALUES ($1, $2, $3, $4, $5, $6, $7)', [id, name, gender, age, email, password, role_id])
            .then(() => {
              console.log('User created successfully')
            }       
         )
            .catch(err => console.error('Error inserting user:', err.stack));
            
+    }
+
+    async getRole_id(role_name) {
+      const result = await pool.query('SELECT role_id FROM Roles WHERE role_name = $1', [role_name]);
+      return result.rows[0].role_id;
     }
 }
 
