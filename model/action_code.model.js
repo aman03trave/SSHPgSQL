beb45876d1977a5bb8ff8426d7f1fb9c4fddd366
aman03trave <aman.panda@learner.manipal.edu>
2025-03-21 16:52:54 +0530

    finally resolved

--- model/action_code.model.js
+++ model/action_code.model.js
@@ -8,10 +8,8 @@ pool
     pool.query(`CREATE TABLE IF NOT EXISTS Action_Code(
         action_code_id SERIAL PRIMARY KEY,
         code VARCHAR(20) NOT NULL UNIQUE
-<<<<<<< HEAD
     )`);
-=======
-    );`);
+
   })
   .then(() => {
     console.log('Action Code table created');
@@ -19,5 +17,4 @@ pool
   .catch((err) => {
     console.error('Error creating Action Code table', err);
     throw err;
->>>>>>> add2f23 (certain changes)
   })
