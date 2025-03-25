5c889aca8cc03e1a8f0e2f057cb923318092ffe8
aman03trave <aman.panda@learner.manipal.edu>
2025-03-25 18:29:51 +0530

    little change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRouter from './routers/user.router.js'
 import formRouter from './routers/form.router.js';
-import errorHandler from './errorMiddleware.js';
+import errorHandler from './middleware/errorMiddleware.js';
 
 const app = express();
 app.use(bodyParser.json());
