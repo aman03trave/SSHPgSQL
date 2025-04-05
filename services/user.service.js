fa9f1fc4474ec401b4a13c7d4d3747030f04fb3e
aman03trave <aman.panda@learner.manipal.edu>
2025-04-05 21:04:16 +0530

    commit

--- services/user.service.js
+++ services/user.service.js
@@ -91,7 +91,7 @@ class Users {
 
     async getUserProfile(userId){
       try {
-        const result = await pool.query('SELECT name, age, phone_no, gender FROM Users WHERE user_id = $1', [userId]);
+        const result = await pool.query('SELECT name, age, phone_no, gender, email FROM Users WHERE user_id = $1', [userId]);
         return result.rows[0];
       } catch (error) {
         throw new Error(`Error getting user : '${userId, error}'`)
